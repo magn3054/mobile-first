59804508bb699bd30b3225460c0c96ae64ff3ac4
magn3054 <mdch20@student.aau.dk>
2024-04-13 21:52:17 +0200

    forside umiddelbart done (mangler tekst)

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -8,11 +8,17 @@ burgerIcon.addEventListener('click', function() {
     if (burgerIcon.src.includes('burger-icon.svg')) {
         burgerIcon.src = 'icons/cross-icon.svg';
         headtext.style.animation = "headertext 1s forwards";
+        sidebar.style.display = "flex";
         sidebar.style.animation = "sidebaranimation 1s forwards";
 
     } else {
         burgerIcon.src = 'icons/burger-icon.svg';
         headtext.style.animation = "headertext-reverse 1.5s forwards";
         sidebar.style.animation = "sidebaranimation-reverse 1s forwards";
+        setTimeout(function() {
+            sidebar.style.display = "none";
+        }, 1000);
+        
+        
     }   
 });
---
+++ js/slideshow.js
@@ -0,0 +1,26 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let indicators = document.getElementsByClassName("indicator");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < indicators.length; i++) {
+    indicators[i].className = indicators[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  indicators[slideIndex-1].className += " active";
+}
