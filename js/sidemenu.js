838bcff49a0ab6a3608ff974f849e6ab91bbaa05
magn3054 <mdch20@student.aau.dk>
2024-04-13 15:05:05 +0200

    header with dropout sidemenu

---
+++ js/sidemenu.js
@@ -0,0 +1,18 @@
+const burgerIcon = document.getElementById('burger');
+const headtext = document.getElementById('headertext');
+const sidebar = document.querySelector('.sidebar');
+const page = document.querySelector('.page');
+
+burgerIcon.addEventListener('click', function() {
+    // Toggle burger icon src
+    if (burgerIcon.src.includes('burger-icon.svg')) {
+        burgerIcon.src = 'icons/cross-icon.svg';
+        headtext.style.animation = "headertext 1s forwards";
+        sidebar.style.animation = "sidebaranimation 1s forwards";
+
+    } else {
+        burgerIcon.src = 'icons/burger-icon.svg';
+        headtext.style.animation = "headertext-reverse 1.5s forwards";
+        sidebar.style.animation = "sidebaranimation-reverse 1s forwards";
+    }   
+});
