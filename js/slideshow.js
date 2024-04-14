a4cad89be4fd0c644f751f0430be973192cc6649
magn3054 <mdch20@student.aau.dk>
2024-04-14 02:37:15 +0200

    vil gerne skifte farverne på bådene men det satme besværligt

--- js/slideshow.js
+++ js/slideshow.js
@@ -12,15 +12,11 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slides");
-  let indicators = document.getElementsByClassName("indicator");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  for (i = 0; i < indicators.length; i++) {
-    indicators[i].className = indicators[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  indicators[slideIndex-1].className += " active";
+
 }
