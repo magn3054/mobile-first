8d962f44ecc6c5e9837c011114109950df67cb8d
magn3054 <mdch20@student.aau.dk>
2024-04-15 08:42:27 +0200

    javascript function names chanegd

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,5 +1,5 @@
 const burgerIcon = document.getElementById('burger');
-const headtext = document.getElementById('headertext');
+const headtext = document.getElementById('wordmark');
 const sidebar = document.querySelector('.sidebar');
 const page = document.querySelector('.page');
 
@@ -7,13 +7,13 @@ burgerIcon.addEventListener('click', function() {
     // Toggle burger icon src
     if (burgerIcon.src.includes('burger-icon.svg')) {
         burgerIcon.src = 'icons/cross-icon.svg';
-        headtext.style.animation = "headertext 1s forwards";
+        headtext.style.animation = "wordmark 1s forwards";
         sidebar.style.display = "flex";
         sidebar.style.animation = "sidebaranimation 1s forwards";
 
     } else {
         burgerIcon.src = 'icons/burger-icon.svg';
-        headtext.style.animation = "headertext-reverse 1.5s forwards";
+        headtext.style.animation = "wordmark-reverse 1.5s forwards";
         sidebar.style.animation = "sidebaranimation-reverse 1s forwards";
         setTimeout(function() {
             sidebar.style.display = "none";
