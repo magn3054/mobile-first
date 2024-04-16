d58b6d333d3212083ddb518199539e7f94eb1a62
magn3054 <mdch20@student.aau.dk>
2024-04-17 01:16:16 +0200

    drop out navbar updated

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,40 +1,22 @@
-const burgerIcon = document.getElementById('burger');
-const headtext = document.getElementById('wordmark');
-const sidebar = document.querySelector('.sidebar');
+function openNav() {
+    document.getElementById("Nav").style.width = "100%";
+}
+  
+function closeNav() {
+    document.getElementById("Nav").style.width = "0%";
+}
 
-burgerIcon.addEventListener('click', function() {
-    // Toggle burger icon src
-    if (burgerIcon.src.includes('burger-icon.svg')) {
-        burgerIcon.src = 'icons/cross-icon.svg';
-        headtext.style.animation = "wordmark 1s forwards";
-        sidebar.style.display = "flex";
-        sidebar.style.animation = "sidebaranimation 1s forwards";
 
+var dropdown = document.getElementsByClassName("dropdown");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    var dropdownContent = this.parentNode.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
     } else {
-        burgerIcon.src = 'icons/burger-icon.svg';
-        headtext.style.animation = "wordmark-reverse 1.5s forwards";
-        sidebar.style.animation = "sidebaranimation-reverse 1s forwards";
-        setTimeout(function() {
-            sidebar.style.display = "none";
-        }, 1000);
-        
-        
-    }   
-});
-
-
-const dropDowns = document.querySelectorAll('.dropplus');
-dropDowns.forEach(dropDown => {
-    const parentElement = dropDown.closest('.undermenu');
-    const dropDownContainer = parentElement.querySelector('.dropdown-container');
-
-    dropDown.addEventListener('click', function() {
-        // Toggle the display of the dropdown container
-        if (dropDownContainer.style.display === "flex") {
-            dropDownContainer.style.display = "none";
-            dropDown.src = 'icons/plus-icon.svg';
-        } else {
-            dropDownContainer.style.display = "flex";
-            dropDown.src = 'icons/minus-icon.svg';
-    }});
-});
+      dropdownContent.style.display = "block";
+    }
+  });
+}
